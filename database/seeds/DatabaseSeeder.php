4893796d37fe1034268d83e7a28ca8a2353f6f0c
ryota <uta09007@yahoo.co.jp>
2018-11-19 13:53:21 +0900

    DB更新

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,16 +12,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::insert("INSERT into items (name,img,description,price) VALUES (?,?,?,?)",[
-            "トマト",
-            "http://placehold.it/200x200&text=tomato",
-            "みずみずしいトマトです。",
+            "カステラ",
+            "http://placehold.it/200x200&text=kasutera",
+            "手作りならではの変わらぬおいしさをお届けします",
+            "100"
+        ]);
+        DB::insert("INSERT into items (name,img,description,price) VALUES (?,?,?,?)",[
+            "ようかん",
+            "http://placehold.it/200x200&text=youkan",
+            "伝統の味はそのままに、気軽にお楽しみ頂ける一口サイズに仕上げました",
             "200"
         ]);
         DB::insert("INSERT into items (name,img,description,price) VALUES (?,?,?,?)",[
-            "レタス",
-            "http://placehold.it/200x200&text=lettuce",
-            "しゃきしゃきしたレタスです。",
-            "150"
+            "まんじゅう",
+            "http://placehold.it/200x200&text=manjuu",
+            "甘すぎない上品な味わいはご年配の方を含め様々な方に愛されています",
+            "300"
+        ]);
+        DB::insert("INSERT into items (name,img,description,price) VALUES (?,?,?,?)",[
+            "どらやき",
+            "http://placehold.it/200x200&text=dorayaki",
+            "ふっくらした小豆の食感、豊かな粒あん、そして栗のほっこりとした歯ごたえと存在感が魅力的です",
+            "400"
         ]);
         // $this->call(UsersTableSeeder::class);
     }
